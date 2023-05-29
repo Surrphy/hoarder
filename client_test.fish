43f158973101accb308406239b1e1d9bc8231a9d
Konrad Kine <konrad.kine@tutanota.com>
2023-05-29 08:12:30 +0200

    chore: add simple test script

---
+++ client_test.fish
@@ -0,0 +1,13 @@
+#!/bin/fish
+
+set output_file /tmp/hoarder_dev_test
+
+set url 'http://localhost:3000'
+
+curl -X GET -d '{"user_fingerprint": "5019 B899 74E8 F56C 2B5B  FA4F EBF8 5543 5E77 07A1"}' -H 'Content-Type: application/json' $url/login --silent > $output_file
+
+set api_token ( gpg -r -a -d -q $output_file)
+
+curl -X GET -d '{"user_fingerprint": "5019 B899 74E8 F56C 2B5B  FA4F EBF8 5543 5E77 07A1","api_token": "'$api_token'"}' -H 'Content-Type: application/json' $url/secret --silent > $output_file
+
+gpg -r -a -d -q $output_file
